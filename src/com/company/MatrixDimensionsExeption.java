2f711c3642a4f76610c24cd1e2e58424ef5e877b
Anastasia <kankankan92@mail.ru>
2019-07-02 17:20:19 +0300

    Добавление комментария

--- src/com/company/MatrixDimensionsExeption.java
+++ src/com/company/MatrixDimensionsExeption.java
@@ -2,10 +2,13 @@ package com.company;
 
 public class MatrixDimensionsExeption extends RuntimeException {
 
-
+    /*
+    Dimensions - размеры.
+     */
     public MatrixDimensionsExeption(String string) {
         super(string);
     }
 
-    public MatrixDimensionsExeption(){}
+    public MatrixDimensionsExeption() {
+    }
 }
